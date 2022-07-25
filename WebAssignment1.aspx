01f397151c0c7eea6ae8337ae45ddeb55f4c7076
Hadiqa Rabnawaz <108441711+guleadan@users.noreply.github.com>
2022-07-25 07:06:34 -0400

    Add files via upload

---
+++ WebAssignment1.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebAssignment1.aspx.cs" Inherits="WebAssignment1.WebAssignment1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Student Data</title>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script>
+        function runAjax() {
+            var selected = document.getElementById("DropDown");
+            $.ajax({
+                url: 'WebAssignment1.aspx.cs/DisplayData',
+                type: 'post',
+                data: JSON.stringify({ "student": selected }),
+                contentType: 'application/json',
+                async: false,
+                error:function () { alert("Ops, Error Occured!") },
+                success: function (data) {
+                    document.getElementById("fname").innerHTML = data.d[0];
+                    document.getElementById("lname").innerHTML = data.d[1];
+                    document.getElementById("rollno").innerHTML =data.d[2];
+                }
+            })
+        }
+            
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div >
+            <h2>Select Sutdent Name</h2>
+            <asp:DropDownList  runat="server" ID="DropDown" width="200px" OnCheckChange="runAjax()" ></asp:DropDownList><br />
+            <asp:Label runat="server">First Name:</asp:Label>
+            <asp:Label runat="server" ID="fname"></asp:Label><br />
+
+            <asp:Label runat="server" >Last Name: </asp:Label>
+            <asp:Label runat="server" ID="lname"></asp:Label><br />
+
+            <asp:Label runat="server">Roll Number:</asp:Label>
+            <asp:Label runat="server" ID="rollno"></asp:Label><br />
+            
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebAssignment1.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.Services;
+using System.Web.UI.WebControls;
+
+namespace WebAssignment1
+{
+    public partial class WebAssignment1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var stulist = StudentList();
+            DropDown.DataSource = stulist;
+            DropDown.DataTextField = "Fname";
+            DropDown.DataValueField = "Rollno";
+            DropDown.DataBind();
+
+
+        }
+        
+        public class Student
+        {
+           public string Fname { get; set; }
+           public string Name { get; set; }
+           public  string Rollno { get; set; }
+           
+        }
+        public static List<Student> StudentList() 
+        {
+            var myList = new List<Student>();
+            myList.Add(new Student() { Fname= "Ali", Name ="Hadir", Rollno="19" });
+            myList.Add(new Student() { Fname = "Hamza", Name = "Mushtaq", Rollno = "33" });
+            myList.Add(new Student() { Fname = "Rida", Name = "Fatima", Rollno = "44" });
+            myList.Add(new Student() { Fname = "Iram", Name = "Fiyaz", Rollno = "43" });
+            myList.Add(new Student() { Fname = "Hadiqa", Name = "Rabnawaz", Rollno = "42" });
+            myList.Add(new Student() { Fname = "Aliya", Name = "Hadi", Rollno = "192" });
+            myList.Add(new Student() { Fname = "Alim", Name = "baloch", Rollno = "59" });
+
+            return myList;
+
+
+        }
+        [WebMethod]
+        public static List<string> DisplayData(string student)
+        {
+            var stulist = StudentList();
+            var dispStudent = new List<string>(); //data of selected student sent back to ajax
+
+            foreach (var attribute in stulist)
+            {
+                if (attribute.Rollno == student)
+                {
+                    dispStudent.Add(attribute.Fname);
+                    dispStudent.Add(attribute.Name);
+                    dispStudent.Add(attribute.Rollno);
+                }
+
+            }
+            return dispStudent;
+        }
+
+
+    }
+}
